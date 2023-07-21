5afed0b150bedc1a70845f62d3546c07ef120a85
Levente Farkas <thebe14@gmail.com>
2023-07-21 13:05:55 +0200

    Added initial frontend app

--- frontend/application/src/main.js
+++ frontend/application/src/main.js
@@ -1,16 +1,5 @@
-import Vue from 'vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-
-// Import Bootstrap and BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
-
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
